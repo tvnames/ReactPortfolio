b71d7b51f229499716bdd3236867e17405248061
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-19 15:42:14 -0700

    Added first section from html portfolio

---
+++ src/components/firstSection.js
@@ -0,0 +1,59 @@
+<!-- About-->
+<section class="page-section bg-primary" id="about">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 text-center">
+                <h2 class="text-white mt-0">I've got what you need!</h2>
+                <hr class="divider light my-4" />
+                <p class="text-white-50 mb-4">My name is Andrew Parsons. I am 30 years old and a recent graudate of the Coding Bootcamp! I have had many previous jobs including being in the Navy, working as a delivery driver overnight, to working in door-to-door sales. I am originally from Indiana and I moved to California in 2014. I have lived south of the border in Tijuana, Mx for several years before meeting my wonderful wife and now we live together in Imperial Beach. I enjoy videogames and coding. I would love to learn how to surf but at this point the pacific ocean is way too cold for me.
+                </p></p>
+                <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Services-->
+<section class="page-section" id="services">
+    <div class="container">
+        <h2 class="text-center mt-0">At Your Service</h2>
+        <hr class="divider my-4" />
+        <div class="row">
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="mt-5">
+                    <i class="fas fa-4x fa-gem text-primary mb-4"></i>
+                    <h3 class="h4 mb-2">HTML In Progress</h3>
+                    <p class="text-muted mb-0">Check out my very first project as a full stack developer</p>              
+                    <p><a href="https://github.com/tvnames/prework-about-me">About me on Github</a></p>
+                      <p><a href="https://tvnames.github.io/prework-about-me/">About Me on the Web</a></p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="mt-5">
+                    <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
+                    <h3 class="h4 mb-2">Javascript in Action!</h3>
+                    <p class="text-muted mb-0">A javascript working password generator!</p>  
+                    <p><a href=https://github.com/tvnames/password_generator>Password generator on Github</a></p>
+                    <p><a href=https://tvnames.github.io/password_generator>Password generator on the Web</a></p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="mt-5">
+                    <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+                    <h3 class="h4 mb-2">AllTrails and Parks</h3>
+                    <p class="text-muted mb-0">A Dev team super starter application!</p>
+                    <p><a href=https://github.com/hdezbriant/AllTrails-AND-Parks>AllTrails and Parks on Github</a></p>
+                    <p><a href=https://hdezbriant.github.io/AllTrails-AND-Parks>AllTrails and Parks on the Web</a></p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 text-center">
+                <div class="mt-5">
+                    <i class="fas fa-4x fa-heart text-primary mb-4"></i>
+                    <h3 class="h4 mb-2">Future Projects</h3>
+                    <p class="text-muted mb-0">I can't wait to work on our first project with you! Please check out my resume and my LinkedIn.</p>
+                   <p><a href=https://drive.google.com/file/d/162gI0CEPwWYpGVWuzF17TeHOJdzXO8a6/view?usp=sharing>My resume</a></p>
+                   <p><a href=https://www.linkedin.com/in/andrew-parsons-654359141>My LinkedIn</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
