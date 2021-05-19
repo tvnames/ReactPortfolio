84ee36e35f759d10ddc7ba8918f6a975e6de9312
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-19 15:37:02 -0700

    Added header information from html portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end">
+                <h1 class="text-uppercase text-white font-weight-bold">Hello World! My name is Andrew!</h1>
+                <hr class="divider my-4" />
+                <img id="profimg" src="assets/profilepic1.jpg"  alt="a picture of me" />
+            </div>
+            
+    </div>
