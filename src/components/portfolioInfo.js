b0f1f49980221152b44392c9b7a162ccd91d6053
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-19 15:44:44 -0700

    Added portfolio info from html portfolio

---
+++ src/components/portfolioInfo.js
@@ -0,0 +1,61 @@
+<!-- Portfolio-->
+<div id="portfolio">
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+            <div class="col-lg-4 col-sm-6">
+                <a class="portfolio-box" href="assets/portfolio/fullsize/1.jpg">
+                    <img class="img-fluid" src="assets/portfolio/thumbnails/1.jpg" alt="..." />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">Category</div>
+                        <div class="project-name">Coming Soon!</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="portfolio-box" href="assets/portfolio/fullsize/2.jpg">
+                    <img class="img-fluid" src="assets/portfolio/thumbnails/2.jpg" alt="..." />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">Web Dev</div>
+                        <div class="project-name">Password Generator</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="portfolio-box" href="assets/portfolio/fullsize/3.jpg">
+                    <img class="img-fluid" src="assets/portfolio/thumbnails/3.jpg" alt="..." />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">Initial Knowledge</div>
+                        <div class="project-name">About Me</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="portfolio-box" href="assets/portfolio/fullsize/4.jpg">
+                    <img class="img-fluid" src="assets/portfolio/thumbnails/4.jpg" alt="..." />
+                    <div class="portfolio-box-caption">
+                        <div class="project-category text-white-50">Group Work</div>
+                        <div class="project-name">AllTrails and Parks</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="portfolio-box" href="assets/portfolio/fullsize/5.jpg">
+                    <img class="img-fluid" src="assets/portfolio/thumbnails/5.jpg" alt="..." />
+                    <div class="portfolio-box-caption">  
+                        <div class="project-category text-white-50"></div>
+                        <div class="project-name">Coming Soon!</div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <a class="portfolio-box" href="assets/portfolio/fullsize/6.jpg">
+                    <img class="img-fluid" src="assets/portfolio/thumbnails/6.jpg" alt="..." />
+                    <div class="portfolio-box-caption p-3">
+                        <div class="project-category text-white-50"></div>
+                        <div class="project-name">Your Request Here!</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
