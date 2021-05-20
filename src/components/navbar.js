3161f3553b92ddb3bdacd176b641b429e706303a
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-19 17:17:50 -0700

    Added bold components for navlinks

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,37 @@
-<!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top"></a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+import React, { Component } from "react";
+import Navitem from "./Navitem";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      NavItemActive: "",
+    };
+  }
+  activeitem = (x) => {
+    if (this.state.NavItemActive.length > 0) {
+      document
+        .getElementById(this.state.NavItemActive)
+        .classList.remove("active");
+    }
+    this.setState({ NavItemId: x }, () => {
+      document.getElementById(this.state.NavItemActive).classList.add("active");
+    });
+  };
+
+  render() {
+    return (
+      <nav>
+        <ul>
+          <Navitem item="About" tolink="/"></Navitem>
+          <Navitem item="Services" tolink="/about"></Navitem>
+          <Navitem item="Portfolio" tolink="/education"></Navitem>
+          <Navitem item="Skills" tolink="/skills"></Navitem>
+          <Navitem item="Contact" tolink="/contact"></Navitem>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
