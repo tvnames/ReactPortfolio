451e34f8ac69b7f5c6d1b212bc7fe3efd2e86c7d
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-06-28 11:51:49 -0700

    Added url's to projects

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -2,53 +2,33 @@ import React, { Component } from "react";
 import Skills from "../contents/Skills";
 
 class CardInfo extends Component {
+  componentDidMount() {
+    window.location.href = "https://github.com/hdezbriant/AllTrails-AND-Parks";
+  }
   render() {
     return (
-      <div class="CardInfo">
-        <div class="compdet">
-          <h3>My Recent Projects</h3>
-          <div className="CardInfo">
-            <div className="compdet">
-              <a
-                href="https://github.com/hdezbriant/AllTrails-AND-Parks"
-                target="_blank"
-                rel="noreferrer"
-              ></a>
-            </div>
-          </div>
-          <div class="CardInfo">
-            <div class="compdet">
-              <a
-                href="https://tvnames.github.io/password_generator"
-                target="_blank"
-                rel="noreferrer"
-              ></a>
-            </div>
-          </div>
-          <div class="CardInfo">
-            <div class="compdet">
-              <a
-                href="https://stormy-shore-76414.herokuapp.com"
-                target="_blank"
-                rel="noreferrer"
-              ></a>
-            </div>
-          </div>
-          <div class="CardInfo">
-            <div class="compdet">
-              <a
-                href="https://tvnames.github.io/ormEcommerce"
-                target="_blank"
-                rel="noreferrer"
-              ></a>
-            </div>
-          </div>
-          <a
-            href="https://github.com/sjlevalley/InfoFountain"
-            target="_blank"
-            rel="noreferrer"
-          ></a>
-        </div>
+      <div>
+        <h4>AllTrails-AND-Parks</h4>
+      </div>
+    );
+  }
+  componentDidMount() {
+    window.location.href = "https://github.com/tvnames/ormEcommerce";
+  }
+  render() {
+    return (
+      <div>
+        <h4>ormEcommerce</h4>
+      </div>
+    );
+  }
+  componentDidMount() {
+    window.location.href = "https://bestmexsd-appage.herokuapp.com/";
+  }
+  render() {
+    return (
+      <div>
+        <h4>Best Mex SD</h4>
       </div>
     );
   }
